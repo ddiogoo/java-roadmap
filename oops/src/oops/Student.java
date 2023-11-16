f7f35ebf6121cf33e6a8f75a65158540d19544cb
diogomsz <diogomrtsa@gmail.com>
2023-11-16 20:23:35 -0300

    Create Student.java

---
+++ oops/src/oops/Student.java
@@ -0,0 +1,15 @@
+package oops;
+
+public class Student {
+	int age;
+	String address;
+	String name;
+	
+	public void study() {
+		// implementation
+	}
+	
+	public void eat() {
+		// implementation
+	}
+}
