f3260f8ada60dd1db84f33234b6762b1d922360e
diogomsz <diogomrtsa@gmail.com>
2023-11-19 14:06:54 -0300

    array reference type

--- variables/src/variables/Variable.java
+++ variables/src/variables/Variable.java
@@ -1,16 +1,18 @@
 package variables;
 
+import java.util.Random;
+
 public class Variable {
 	public static void main(String[] args) {
-		String s1 = "hello";
-		String s2 = "hello";
-		String s3 = new String("hello");
+		Random random = new Random();
+		int[] array = new int[5];
 		
-		System.out.println(s1 == s2);
-		System.out.println(s1 == s3);
-		System.out.println(s2 == s3);
+		for(int i = 0; i < array.length; i++) {
+			array[i] = random.nextInt(0, array.length) + 1;
+		}
 		
-		Person e = new Employee();
-		System.out.println(e.profession());
+		for(int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
 	}
 }
