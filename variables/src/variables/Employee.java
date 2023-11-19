cf013d1ce15118c5ce6d17da16a5366eb7daed03
diogomsz <diogomrtsa@gmail.com>
2023-11-19 11:35:38 -0300

    testando alterar valores passado como valor

---
+++ variables/src/variables/Employee.java
@@ -0,0 +1,13 @@
+package variables;
+
+public class Employee {
+	int empId;
+	
+	public int getEmpId() {
+		return empId;
+	}
+	
+	public void setEmpId(int paramEmpId) {
+		this.empId = paramEmpId;
+	}
+}
--- variables/src/variables/Variable.java
+++ variables/src/variables/Variable.java
@@ -1,40 +1,36 @@
 package variables;
 
-import java.math.BigDecimal;
-
 public class Variable {
 	public static void main(String[] args) {
-		char var1 = 'a';
-		System.out.println(var1);
+		Employee e = new Employee();
+		e.setEmpId(21);
+		int intValue = 21;
+		Integer integerValue = 21;
 		
-		char var2 = 97; 
-		System.out.println(var2);
+		System.out.println("before change values");
+		System.out.println(e.getEmpId());
+		System.out.println(intValue);
+		System.out.println(integerValue);
 		
-		byte var3 = 127;
-		System.out.println(var3);
+		changeEmployeeId(e);
+		changeIntValue(intValue);
+		changeIntegerValue(integerValue);
 		
-		byte n1 = 10;
-		System.out.println(n1);
-		
-		short n2 = n1;
-		System.out.println(n2);
-		
-		int n3 = n2;
-		System.out.println(n3);
-		
-		long n4 = n3;
-		System.out.println(n4);
-		
-		int integerVar = 128;
-		System.out.println(integerVar);
-		
-		byte byteVar = (byte)integerVar;
-		System.out.println(byteVar);
-		
-		float f1 = .7f;
-		System.out.println((double)f1);
-		
-		BigDecimal bd = new BigDecimal(.7f);
-		System.out.println(bd.floatValue());
+		System.out.println("after change values");
+		System.out.println(e.getEmpId());
+		System.out.println(intValue);
+		System.out.println(integerValue);
+	}
+	
+	public static void changeEmployeeId(Employee e) {
+		e.setEmpId(22);
+	}
+	
+	public static void changeIntValue(int intValue) {
+		intValue = 22;
+	}
+	
+	public static void changeIntegerValue(Integer integerValue) {
+		integerValue = 22;
 	}
 }
