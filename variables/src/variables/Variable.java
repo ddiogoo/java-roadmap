c1a90eaa59437e32ec9b56864dc4818c020755ce
diogomsz <diogomrtsa@gmail.com>
2023-11-19 12:15:34 -0300

    compare string java

--- variables/src/variables/Variable.java
+++ variables/src/variables/Variable.java
@@ -2,57 +2,12 @@ package variables;
 
 public class Variable {
 	public static void main(String[] args) {
-		Employee e = new Employee();
-		e.setEmpId(21);
-		int intValue = 21;
-		Integer integerValue = 21;
+		String s1 = "hello";
+		String s2 = "hello";
+		String s3 = new String("hello");
 		
-		System.out.println("before change values");
-		System.out.println(e.getEmpId());
-		System.out.println(intValue);
-		System.out.println(integerValue);
-		
-		changeEmployeeId(e);
-		changeIntValue(intValue);
-		changeIntegerValue(integerValue);
-		
-		System.out.println("after change values");
-		System.out.println(e.getEmpId());
-		System.out.println(intValue);
-		System.out.println(integerValue);
-		
-		System.out.println("compare two reference data types");
-		Employee e1 = new Employee();
-		Employee e2 = new Employee();
-		
-		e1.setEmpId(1);
-		e2.setEmpId(2);
-		System.out.println(e1 == e2);
-		System.out.println(e1.getEmpId() + " <-> " + e2.getEmpId());
-		
-		e1 = e2;
-		System.out.println(e1 == e2);
-		System.out.println(e1.getEmpId() + " <-> " + e2.getEmpId());
-		
-		String str1 = "Diogo Martins de Assis";
-		System.out.println(str1);
-		changeStringValue(str1);
-		System.out.println(str1);
-	}
-	
-	public static void changeEmployeeId(Employee e) {
-		e.setEmpId(22);
-	}
-	
-	public static void changeIntValue(int intValue) {
-		intValue = 22;
-	}
-	
-	public static void changeIntegerValue(Integer integerValue) {
-		integerValue = 22;
-	}
-	
-	public static void changeStringValue(String stringValue) {
-		stringValue = "Assis de Martins Diogo";
+		System.out.println(s1 == s2);
+		System.out.println(s1 == s3);
+		System.out.println(s2 == s3);
 	}
 }
