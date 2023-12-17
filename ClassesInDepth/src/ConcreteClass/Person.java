dfec8b27e9437699b8453543e7b49ee611fe7604
Diogo Martins de Assis <74463348+diogomrtsa@users.noreply.github.com>
2023-12-17 15:10:44 -0300

    Create Person.java

---
+++ ClassesInDepth/src/ConcreteClass/Person.java
@@ -0,0 +1,13 @@
+package ConcreteClass;
+
+public class Person {
+    int empID;
+
+    public Person(int empID) {
+        this.empID = empID;
+    }
+
+    public int getEmpID() {
+        return this.empID;
+    }
+}
