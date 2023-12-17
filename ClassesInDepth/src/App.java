570af5eeb7af57477d77638c7cc7b8b4b1d6065f
Diogo Martins de Assis <74463348+diogomrtsa@users.noreply.github.com>
2023-12-17 15:10:52 -0300

    Update App.java

--- ClassesInDepth/src/App.java
+++ ClassesInDepth/src/App.java
@@ -1,5 +1,17 @@
+import ConcreteClass.Person;
+import ConcreteClass.Rectangle;
+import ConcreteClass.Shape;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        concreteClasses();
+    }
+
+    private static void concreteClasses() {
+        Person person = new Person(21);
+        System.out.println(person.getEmpID());
+
+        Shape rectangle = new Rectangle();
+        rectangle.computeArea();
     }
 }
