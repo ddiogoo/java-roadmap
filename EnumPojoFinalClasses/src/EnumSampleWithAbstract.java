fb78bd97d2b121387d6158fa78cbbf34cac2721b
Diogo Martins de Assis <74463348+diogomrtsa@users.noreply.github.com>
2023-12-23 23:21:08 -0300

    Update EnumSampleWithAbstract.java

--- EnumPojoFinalClasses/src/EnumSampleWithAbstract.java
+++ EnumPojoFinalClasses/src/EnumSampleWithAbstract.java
@@ -1,2 +1,46 @@
 public enum EnumSampleWithAbstract {
+    MONDAY {
+        @Override
+        public void dummyMethod() {
+            System.out.println("MONDAY");
+        }
+    },
+    TUESDAY {
+        @Override
+        public void dummyMethod() {
+            System.out.println("TUESDAY");
+        }
+    },
+    WEDNESDAY {
+        @Override
+        public void dummyMethod() {
+            System.out.println("WEDNESDAY");
+        }
+    },
+    THURSDAY {
+        @Override
+        public void dummyMethod() {
+            System.out.println("THURSDAY");
+        }
+    },
+    FRIDAY {
+        @Override
+        public void dummyMethod() {
+            System.out.println("FRIDAY");
+        }
+    },
+    SATURDAY {
+        @Override
+        public void dummyMethod() {
+            System.out.println("SATURDAY");
+        }
+    },
+    SUNDAY {
+        @Override
+        public void dummyMethod() {
+            System.out.println("SUNDAY");
+        }
+    };
+
+    public abstract void dummyMethod();
 }
