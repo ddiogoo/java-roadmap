3fe5b67efa53e99ad6f7d647155236262b5d14ce
Diogo Martins de Assis <74463348+diogomrtsa@users.noreply.github.com>
2023-12-23 23:13:42 -0300

    Update EnumSample.java

--- EnumPojoFinalClasses/src/EnumSample.java
+++ EnumPojoFinalClasses/src/EnumSample.java
@@ -1,2 +1,17 @@
 public enum EnumSample {
+    MONDAY(101, "1st day of the week"),
+    TUESDAY(102, "2st day of the week"),
+    WEDNESDAY(103, "3st day of the week"),
+    THURSDAY(104, "4st day of the week"),
+    FRIDAY(105, "5st day of the week"),
+    SATURDAY(106, "its 1st weekoff"),
+    SUNDAY(107, "its 2st weekoff");
+
+    private int val;
+    private String comment;
+
+    EnumSample(int val, String comment) {
+        this.val = val;
+        this.comment = comment;
+    }
 }
