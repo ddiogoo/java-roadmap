4d7a41cadb720ae5c7b5012dbf5e88b7dd761968
Diogo Martins de Assis <74463348+diogomrtsa@users.noreply.github.com>
2023-12-23 23:17:37 -0300

    Update OtherEnumSample.java

--- EnumPojoFinalClasses/src/OtherEnumSample.java
+++ EnumPojoFinalClasses/src/OtherEnumSample.java
@@ -1,4 +1,10 @@
 public enum OtherEnumSample {
+    MONDAY {
+        @Override
+        public void dummyMethod() {
+            System.out.println("monday dummy method");
+        }
+    },
 
     public void dummyMethod() {
         System.out.println("default dummy method");
